d9aa67e5e8e923468252c3cdbd8abbb98291859a
largenumbergoeshere <largenumbergoeshere@gmail.com>
2026-07-08 14:44:59 +1000

    inital commit

---
+++ src/main.c
@@ -0,0 +1,199 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "raylib.h"
+#include "raymath.h"
+#include <stdlib.h>
+#ifdef PLATFORM_WEB
+	#include "emscripten/emscripten.h"
+#endif
+
+#define lassert(condition) \
+do {	\
+	if (condition) {	\
+		printf("Assertion failed at %s:%i\n", __FILE__, __LINE__);	\
+		exit(1);	\
+	};	\
+} while (0);
+
+/*
+	https://itch.io/jam/raylib-6x-gamejam
+	The goal of this gamejam is making a game with raylib in 6 DAYS with some technical constraints and following proposed theme.
+	Theme: hex + merge
+
+	Hard constraints (mandatory requirements):
+
+		Game MUST be 720x720 pixels resolution!
+		Game MUST be run on Web with WebAssembly technology
+		Game MUST be under 64MB (full package: .wasm + .data)
+
+
+ */
+
+// #ifdef PLATFORM_WEB
+//     #include <emscripten/emscripten.h>
+// #endif
+
+// typedef struct Grid {
+//
+// };
+
+
+
+static Rectangle backingTextureSize = {.x=0, .y=0,.width= 1000, .height=1000};
+static Rectangle destinationScreenSize = {0, 0, 720, 720};
+
+static RenderTexture2D backingTexture = {0};
+
+typedef enum clickableKind_t {
+	ClickableKindUp =		1,
+	ClickableKindDown =		2,
+	ClickableKindLeft =		3,
+	ClickableKindRight =	4,
+} ClickableKind;
+
+typedef struct {
+	Rectangle position;
+	ClickableKind kind;
+} Clickable;
+
+#define MAX_CLICKABLE 64
+static Clickable clickables[MAX_CLICKABLE];
+static int64_t clickableCount = 0;
+
+void initClickables() {
+	Clickable upArrow = {.kind = ClickableKindUp, .position = {0, 0, 20, 20}};
+	Clickable downArrow = {.kind = ClickableKindDown, .position = {0, 30, 20, 20}};
+	Clickable leftArrow = {.kind = ClickableKindLeft, .position = {0, 60, 20, 20}};
+	Clickable rightArrow = {.kind = ClickableKindRight, .position = {0, 90, 20, 20}};
+
+	clickables[clickableCount++] = upArrow;
+	clickables[clickableCount++] = downArrow;
+	clickables[clickableCount++] = leftArrow;
+	clickables[clickableCount++] = rightArrow;
+}
+
+void drawClickables() {
+	for (int i = 0; i < clickableCount; i++) {
+		DrawRectangleRec(clickables[i].position, BROWN);
+	}
+}
+
+void preinit() {
+
+}
+
+void rayinit() {
+	InitWindow(destinationScreenSize.width, destinationScreenSize.height, "game");
+}
+
+void init() {
+	backingTexture = LoadRenderTexture(backingTextureSize.width, backingTextureSize.height);
+	initClickables();
+}
+
+void drawGrid() {
+	float offX = 100;
+	float offY = 100;
+	float width = 125;
+	float height = 125;
+	float padx = 10;
+	float pady = 10;
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			Rectangle rec = {.x = (float)i * width + offX + (float)i * padx, .y = (float)j * height + offY + (float)j * pady, width, height};
+			DrawRectangleRec(rec, WHITE);
+		}
+	}
+}
+
+
+
+void draw() {
+	BeginTextureMode(backingTexture);
+	{
+		drawGrid();
+		drawClickables();
+		DrawText("Title", 450, 25, 55, RED);
+
+
+		ClearBackground(GREEN);
+	}
+	EndTextureMode();
+
+
+	BeginDrawing();
+	{
+		Rectangle flippedSource = backingTextureSize;
+		flippedSource.height = -flippedSource.height;
+		DrawTexturePro(backingTexture.texture, flippedSource, destinationScreenSize, Vector2Zero(), 0, WHITE);
+	}
+	EndDrawing();
+}
+
+
+
+Vector2 getMousePos() {
+	Vector2 absPos = GetMousePosition();
+	float unitx = absPos.x / destinationScreenSize.width;
+	float unity = absPos.y / destinationScreenSize.height;
+
+	float relx = unitx * backingTextureSize.width;
+	float rely = unity * backingTextureSize.height;
+
+	Vector2 relPos = {relx, rely};
+
+	return relPos;
+}
+
+void tickClickables () {
+	Vector2 mouse = getMousePos();
+	bool wasLeftclickPressed = IsMouseButtonReleased(MOUSE_LEFT_BUTTON);
+
+	for (int i = 0; i < clickableCount; i++) {
+		bool isButtonHovered = CheckCollisionPointRec(mouse, clickables[i].position);
+		if (wasLeftclickPressed) {
+			if (isButtonHovered) {
+				printf("I am clicked! %i \n", i);
+
+				switch (clickables[i].kind) {
+					case ClickableKindDown: {} break;
+					case ClickableKindUp: {} break;
+					case ClickableKindLeft: {} break;
+					case ClickableKindRight: {} break;
+					default: lassert(false);
+
+				}
+			}
+		}
+	}
+}
+
+void tick() {
+	tickClickables();
+}
+
+
+void update() {
+	draw();
+	tick();
+}
+
+// bootstrap the game with different frontentds
+#ifndef _DEBUG
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+int main() {
+	preinit();
+	rayinit();
+	init();
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+#ifdef PLATFORM_WEB
+	// tell emscripten's js runtime to periodically call the function.
+	// Ensures that the runtime is never blocked
+	emscripten_set_main_loop(update, 60, true);
+#else
+	SetTargetFPS(60);
+	while (!WindowShouldClose()) update();
+#endif
+	return 0;
+}
